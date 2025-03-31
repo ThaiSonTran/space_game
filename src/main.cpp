a8235f57d4d039b43840c53b5ca62d278db88561
ThaiSonTran <sontransinh@gmail.com>
2025-03-31 17:29:39 +0700

    intialize sdl_subsystem, create window & renderer

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,48 @@
-#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <stdio.h>
 
-using namespace std;
+const int WINDOW_WIDTH = 1200;
+const int WINDOW_HEIGHT = 800;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+bool initGame(){
+    if(SDL_Init(SDL_INIT_EVERYTHING) < 0) return false;
+
+
+    int imgFlags = IMG_INIT_PNG;
+    if((IMG_Init(imgFlags) & imgFlags) == 0) return false;
+
+
+    if(SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) == 0)
+        printf("Warning: Linear texture filtering not enabled!");
+
+
+    SDL_Window *gameWindow = SDL_CreateWindow(
+        "My SDL2 Application",
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        WINDOW_WIDTH,
+        WINDOW_HEIGHT,
+        SDL_WINDOW_SHOWN
+    );
+    if(gameWindow == NULL) return false;
+
+
+    SDL_Renderer* gameRenderer = SDL_CreateRenderer(
+        gameWindow,
+        -1, // Driver index (-1 chooses the first available)
+        SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC
+    );
+    if(gameRenderer == NULL) return false;
+
+
+    return true;
+}
+int main( int argc, char* args[]){
+    if(!initGame()){
+        printf("Failed to initialize game");
+        return 1;
+    }
     return 0;
 }
